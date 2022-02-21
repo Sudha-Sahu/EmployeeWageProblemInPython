c65e42855b8bbd40dc05000bebc0878aee694c82
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-21 14:32:52 +0530

    sudha-added checking employee attendance

--- emp_wage.py
+++ emp_wage.py
@@ -1,2 +1,11 @@
+# UC1-checking employee attendance
+
+import random
 
 print("Welcome to employee wage problem")
+emp_present = 1
+attendance = random.randint(0, 1)
+if attendance == emp_present:
+    print("Employee is present")
+else:
+    print("Employee is absent")
