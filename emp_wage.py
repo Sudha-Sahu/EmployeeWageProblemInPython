fa4c365f81010d2fc49f076f01cab327b64413ca
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-22 11:02:38 +0530

    sudha-added storing daily wage

--- emp_wage.py
+++ emp_wage.py
@@ -1,4 +1,4 @@
-# UC7-refactoring the employee wage using functions
+# UC8-storing daily wage
 
 import random
 
@@ -11,20 +11,25 @@ MAX_WORKING_HOUR_PER_MONTH = 100
 
 
 # defining a function
-def get_total_worked_hrs():
+def store_daily_wage():
     day_count = 1
     total_working_hr = 0
+    store_per_day_wage = []
     while day_count <= MAX_WORKING_DAY_PER_MONTH and total_working_hr < MAX_WORKING_HOUR_PER_MONTH:
         is_present = random.randint(0, 2)
         if is_present == IS_PART_TIME:
             total_working_hr += 4
+            daily_hr = 4
         elif is_present == IS_FULL_TIME:
             total_working_hr += 8
+            daily_hr = 8
         else:
             total_working_hr += 0
-        print("total working hrs till day ", day_count, " is : ", total_working_hr)
+            daily_hr = 0
+        daily_wage = daily_hr * WAGE_PER_HR
+        store_per_day_wage.append(daily_wage)
         day_count += 1
-
+    print("day wise wage is :", store_per_day_wage)
     total_wage = total_working_hr * WAGE_PER_HR
     print("Employee total monthly wage is : ", total_wage)
 
@@ -32,4 +37,4 @@ def get_total_worked_hrs():
 print("Welcome to employee wage problem")
 
 # calling a function
-get_total_worked_hrs()
+store_daily_wage()
