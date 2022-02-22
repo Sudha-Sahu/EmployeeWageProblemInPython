4051ac51defaa05afeabdbb654936c24ba756571
Sudha Sahu <sudhasahu52@gmail.com>
2022-02-22 10:36:25 +0530

    sudha-added solving using switch case statement and calculating wage

--- emp_wage.py
+++ emp_wage.py
@@ -1,32 +1,26 @@
-# UC1-checking employee attendance
+# UC4-solving using switch case statement and calculating wage
 
 import random
 
-print("Welcome to employee wage problem")
-emp_present = 1
-attendance = random.randint(0, 1)
-if attendance == emp_present:
-    print("Employee is present")
-else:
-    print("Employee is absent")
-# UC3-adding part time employee and calculating wage
-
-import random
-
+# Constants
 WAGE_PER_HR = 20
-IS_PART_TIME = 1
-IS_FULL_TIME = 2
-working_day_hr = 0
+PART_TIME_HR = 4
+FULL_DAY_HR = 8
 
 print("Welcome to employee wage problem")
+
+# calculating the daily employee wage
+wage_full_day = WAGE_PER_HR * FULL_DAY_HR
+print('Wage per day in full time hr is : {}'.format(wage_full_day))
+
+# calculating the part-time employee wage
+part_time_emp_wage = WAGE_PER_HR * PART_TIME_HR
+print('Part Time Wage per day is : {}'.format(part_time_emp_wage))
 
 is_present = random.randint(0, 2)
-if is_present == IS_PART_TIME:
-    working_day_hr = 4
-elif is_present == IS_FULL_TIME:
-    working_day_hr = 8
-else:
-    working_day_hr = 0
-
-daily_wage = working_day_hr * WAGE_PER_HR
-print("Employee daily wage : ", daily_wage)
+wage = {
+    0: {'is_present': 'absent', 'wage': 0, 'worked_hr': 0},
+    1: {'is_present': 'present-full-day', 'wage': wage_full_day, 'worked_hr': FULL_DAY_HR},
+    2: {'is_present': 'part-time', 'wage': part_time_emp_wage, 'worked_hr': PART_TIME_HR}
+}
+print("Employee daily wage : ", wage.get(is_present))
