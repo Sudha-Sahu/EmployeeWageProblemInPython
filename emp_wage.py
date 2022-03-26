317dac527ddf1950b570d4f1ceb505c7750ca9c5
Sudha Sahu <sudhasahu52@gmail.com>
2022-03-26 17:28:57 +0530

    [sudha]-refactored the code using oops concept

--- emp_wage.py
+++ emp_wage.py
@@ -2,10 +2,14 @@
 
 import random
 
-print("Welcome to employee wage problem")
-emp_present = 1
-attendance = random.randint(0, 1)
-if attendance == emp_present:
-    print("Employee is present")
-else:
-    print("Employee is absent")
+if __name__ == "__main__":
+    try:
+        print("Welcome to employee wage problem")
+        emp_present = 1
+        attendance = random.randint(0, 1)
+        if attendance == emp_present:
+            print("Employee is present")
+        else:
+            print("Employee is absent")
+    except Exception:
+        print("some thing went wrong please check")
